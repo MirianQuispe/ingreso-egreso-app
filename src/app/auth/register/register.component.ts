e3617c00baab151a79f78d81d314e3dec4019166
MirianQuispe <quispemiriamaldana@gmail.com>
2021-05-27 16:58:25 -0300

    implementacion de register user

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RegisterComponent implements OnInit {
+  registroForm : FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.registroForm = this.fb.group({
+      nombre  : ['', Validators.required],
+      correo  : ['', [Validators.required,Validators.email]],
+      password: ['', Validators.required]
+    });
   }
 
+  crearUsuario(){
+    const {nombre, correo, password } = this.registroForm.value;
+
+    this.authService.crearUsuario(nombre, correo, password)
+    .then( credenciales => {
+      console.log(credenciales);
+    })
+    .catch( err => console.error(err));
+  }
 }
