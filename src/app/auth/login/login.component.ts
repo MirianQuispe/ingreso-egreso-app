20ee33f5cfa5916cd5f9a3aa9868d4b61cf65d67
MirianQuispe <quispemiriamaldana@gmail.com>
2021-05-27 16:58:04 -0300

    implementacion de login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm : FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      correo  : ['', [Validators.required,Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  loginUsuario(){
+    if (this.loginForm.valid){
+      const {correo, password } = this.loginForm.value;
+
+      this.authService.loginUsuario(correo, password)
+      .then(rta => {
+          console.log("logueado");
+          this.router.navigate(['']);
+      }).catch( err => {
+        console.log(err);
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: err.message
+        })
+      })
+    }
   }
 
 }
