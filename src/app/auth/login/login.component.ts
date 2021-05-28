05a5bf7b591de06a151947dd582ca3d4a1d93595
MirianQuispe <quispemiriamaldana@gmail.com>
2021-05-27 22:05:34 -0300

    implementacion del sweet alert en login y register

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -26,22 +26,32 @@ export class LoginComponent implements OnInit {
   }
 
   loginUsuario(){
-    if (this.loginForm.valid){
+    console.log("login usuario")
+    if (this.loginForm.invalid){ return;}
+      Swal.fire({
+        title: 'Por favor espere',
+        didOpen: () => {
+          Swal.showLoading()
+          
+        }
+      });
+
       const {correo, password } = this.loginForm.value;
 
       this.authService.loginUsuario(correo, password)
-      .then(rta => {
-          console.log("logueado");
-          this.router.navigate(['']);
-      }).catch( err => {
-        console.log(err);
-        Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: err.message
+        .then(rta => {
+              Swal.close();
+              this.router.navigate(['']);
+            })
+        .catch( err => {
+              console.log(err);
+              Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: err.message
+              })
         })
-      })
     }
-  }
+  
 
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from 'src/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +14,8 @@ export class RegisterComponent implements OnInit {
   registroForm : FormGroup;
 
   constructor(private fb: FormBuilder,
-              private authService: AuthService) { }
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.registroForm = this.fb.group({
@@ -23,12 +26,29 @@ export class RegisterComponent implements OnInit {
   }
 
   crearUsuario(){
+    Swal.fire({
+      title: 'Por favor espere',
+      didOpen: () => {
+        Swal.showLoading()
+        
+      }
+    });
     const {nombre, correo, password } = this.registroForm.value;
 
     this.authService.crearUsuario(nombre, correo, password)
     .then( credenciales => {
       console.log(credenciales);
+      Swal.close();
+      this.router.navigateByUrl("/login");
+
     })
-    .catch( err => console.error(err));
+    .catch( err => {
+      console.error(err);
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: err.message
+      })
+    });
   }
 }
